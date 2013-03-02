4b15492d9fc01aec33e03e4c13879045c6ffc0b5
Paul Seiffert <paul.seiffert@gmail.com>
2013-03-02 21:52:37 +0100

    Added test account. This account is added to the database every time the application is launched without any accounts present in the database.

--- app/Global.scala
+++ app/Global.scala
@@ -3,4 +3,12 @@ import play.api._
 import models._
 import anorm._
 
-object Global extends GlobalSettings
+object Global extends GlobalSettings {
+  override def onStart(app: Application) {
+    if (Account.findAll.isEmpty) {
+      Account.create(
+        Account("1", "paul", "test", "Paul Seiffert", Administrator)
+      )
+    }
+  }
+}
